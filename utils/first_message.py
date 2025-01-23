83b5fb7ba652e411b6162a0e9f54ad623a69c126
qZhenya-dev <evg.mt@mail.ru>
2025-01-23 11:58:17 +0700

    upd

--- utils/first_message.py
+++ utils/first_message.py
@@ -3,7 +3,7 @@ from rich.console import Console
 
 def first_message():
     console = Console()
-    console.print(text2art("NEXT SOFTS\nUNICHAIN", font="tarty1"), style="rgb(235,160,63)")
+    console.print(text2art("NEXT SOFTS\nUNICHAIN\nCOMBINE", font="tarty1"), style="rgb(235,160,63)")
     print()
     console.print("Телеграм с обновлениями и другими софтами:", style="rgb(86,171,227)", end=" ")
     console.print("https://t.me/next_softs\n", style="rgb(86,171,227)")
